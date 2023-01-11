33facf9f0eeac68edaf1ac9dec1e50a597bbebdf
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-01-11 16:37:26 +0100

    Add migrations update

---
+++ TimeManager.ProcessingEngine/Migrations/20230111153718_TaskSetRecordUpdate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeManager.ProcessingEngine.Migrations
+{
+    public partial class TaskSetRecordUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AvgExecutionTime",
+                table: "TaskSetRecords",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Efficiency",
+                table: "TaskSetRecords",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FailedTasks",
+                table: "TaskSetRecords",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SuccededTasks",
+                table: "TaskSetRecords",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgExecutionTime",
+                table: "TaskSetRecords");
+
+            migrationBuilder.DropColumn(
+                name: "Efficiency",
+                table: "TaskSetRecords");
+
+            migrationBuilder.DropColumn(
+                name: "FailedTasks",
+                table: "TaskSetRecords");
+
+            migrationBuilder.DropColumn(
+                name: "SuccededTasks",
+                table: "TaskSetRecords");
+        }
+    }
+}
