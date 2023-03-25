d431cfb6892bcf67dffff9cb5eeb69328891ef81
Arkadiusz Staśko <arek.stasko@gmail.com>
2023-03-25 18:30:37 +0100

    Delete test project

---
+++ TimeManager.ProcessingEngine.Tests/TaskProcessorTests.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TimeManager.ProcessingEngine.Data;
+using TimeManager.ProcessingEngine.Processors;
+
+namespace TimeManager.ProcessingEngine.Tests;
+
+public class Tests
+{
+    //TODO add unit tests after data refactor
+}
